7c8c5083f5bb21aadff957ae90aa5ec3fa11599c
Madison Lynch <madi@mxdi.xyz>
2023-05-10 11:06:09 -0700

    updated config scripts

--- dwm/conf.sh
+++ dwm/conf.sh
@@ -3,15 +3,15 @@
 # https://deadgirl.dev
 
 USR=$(logname)
-DIR=/home/$USR/.config/slstatus
+DIR=/home/$USR/.config/dwm
 
 if [ "$1" == "init" ]; then
 	printf "Creating scripts directory at %s\n" $DIR
 	mkdir -p $DIR
-	cp -f dynbat.sh $DIR
+	cp -f scripts/* $DIR
 
 	printf "Transferring ownership to %s\n" $USR
-	chown -R $USR:$(groups $USR) $DIR
+	chown -R $USR:$(groups $USR) $DIR .
 	chmod 755 $DIR/*
 elif [ "$1" == "remove" ]; then
 	printf "Removing scripts directory at %s\n" $DIR
--- slstatus/conf.sh
+++ slstatus/conf.sh
@@ -3,16 +3,16 @@
 # https://deadgirl.dev
 
 USR=$(logname)
-DIR=/home/$USR/.config/dwm
+DIR=/home/$USR/.config/slstatus
 
 if [ "$1" == "init" ]; then
 	printf "Creating scripts directory at %s\n" $DIR
 	mkdir -p $DIR
-	cp -f scripts/* $DIR
+	cp -f dynbat.sh $DIR
 
 	printf "Transferring ownership to %s\n" $USR
 	chown -R $USR:$(groups $USR) $DIR
-	chmod 755 $DIR/*
+	chmod 755 $DIR/* .
 elif [ "$1" == "remove" ]; then
 	printf "Removing scripts directory at %s\n" $DIR
 	rm -rf $DIR
